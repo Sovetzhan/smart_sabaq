87a918bf63b63ab8f6306d09957e2a226136d5b9
Sovetzhanuly_Sayat <ssovetzhanov03@gmail.com>
2026-02-04 20:14:38 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:smart_sabaq/screens/teachers/add_teacher_screen.dart';
 
 import 'services/auth_service.dart';
 import 'screens/auth/login_screen.dart';
@@ -24,7 +25,7 @@ class AdminApp extends StatelessWidget {
         useMaterial3: true,
         colorSchemeSeed: Colors.blueGrey,
       ),
-      home: const AuthGate(),
+      home: TeachersScreen(),
     );
   }
 }
@@ -33,57 +34,57 @@ class AdminApp extends StatelessWidget {
 /// AuthGate — точка входа в приложение
 /// решает: логин или основной экран
 ///
-class AuthGate extends StatelessWidget {
-  const AuthGate({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final authService = AuthService();
-
-    return StreamBuilder<User?>(
-      stream: authService.authStateChanges(),
-      builder: (context, snapshot) {
-        // Firebase думает
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        }
-
-        // Не залогинен
-        if (!snapshot.hasData) {
-          return LoginScreen();
-        }
-
-        // Залогинен → грузим профиль
-        return FutureBuilder<Map<String, dynamic>>(
-          future: authService.getUserProfile(snapshot.data!.uid),
-          builder: (context, userSnap) {
-            if (!userSnap.hasData) {
-              return const Scaffold(
-                body: Center(child: CircularProgressIndicator()),
-              );
-            }
-
-            final role = userSnap.data!['role'];
-
-            // MVP: пока только админ
-            if (role == 'admin') {
-              return TeachersScreen();
-            }
-
-            // Заглушка для teacher (позже сделаем TeacherApp)
-            return const Scaffold(
-              body: Center(
-                child: Text(
-                  'Teacher App пока не реализован',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-            );
-          },
-        );
-      },
-    );
-  }
-}
+// class AuthGate extends StatelessWidget {
+//   const AuthGate({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final authService = AuthService();
+//
+//     return StreamBuilder<User?>(
+//       stream: authService.authStateChanges(),
+//       builder: (context, snapshot) {
+//         // Firebase думает
+//         if (snapshot.connectionState == ConnectionState.waiting) {
+//           return const Scaffold(
+//             body: Center(child: CircularProgressIndicator()),
+//           );
+//         }
+//
+//         // Не залогинен
+//         if (!snapshot.hasData) {
+//           return LoginScreen();
+//         }
+//
+//         // Залогинен → грузим профиль
+//         return FutureBuilder<Map<String, dynamic>>(
+//           future: authService.getUserProfile(snapshot.data!.uid),
+//           builder: (context, userSnap) {
+//             if (!userSnap.hasData) {
+//               return const Scaffold(
+//                 body: Center(child: CircularProgressIndicator()),
+//               );
+//             }
+//
+//             final role = userSnap.data!['role'];
+//
+//             // MVP: пока только админ
+//             if (role == 'admin') {
+//               return TeachersScreen();
+//             }
+//
+//             // Заглушка для teacher (позже сделаем TeacherApp)
+//             return const Scaffold(
+//               body: Center(
+//                 child: Text(
+//                   'Teacher App пока не реализован',
+//                   style: TextStyle(fontSize: 16),
+//                 ),
+//               ),
+//             );
+//           },
+//         );
+//       },
+//     );
+//   }
+// }
